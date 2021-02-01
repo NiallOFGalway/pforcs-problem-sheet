6393f9ad0993e117783ed13e3e15468b67a5a056
NiallOFGalway <G00398819@gmit.ie>
2021-02-01 12:11:30 +0000

    Week 02 - Weekly Task - BMI

--- bmi.py
+++ bmi.py
@@ -4,15 +4,20 @@
 # Reference: https://stackoverflow.com/questions/20405610/bmi-calculator-in-python/20405792#:~:text=')%20while%20input%20%3D%3D%20'imperial,which%20means%20you%20are%20underweight.
 # Reference: https://stackoverflow.com/questions/35520965/how-do-i-let-the-user-input-enter-a-decimal-number-as-well-as-just-an-integer/35521117
 # Reference: https://www.w3schools.com/python/python_string_formatting.asp
+# Reference: Liaising with classmates
 
 weight = int(input('Enter Weight in KG: '))
-height = float(input('Enter Height in Meters: '))
+height = float(input('Enter Height in CM: '))
 # The above asks the user to input required information
 # float must be used for height as we are working in meters hence require the use of decimals, otherwise only a whole number would be accepted.
 
-result = weight/(height*height)
+# result = weight/(height*height)
+# The above code would work if height input was in M, but would not work for CM
+
+result = (weight / height / height) * 10000
+# After a bit of fiddling about and communicating with classmates, the above works for inputting height in CM
 
 print ('Your BMI is: {:.2f}'.format(result))
-# This took quite a bit of de-bugging as I had to figure out how to be able to add both text and the result to the same line
+# This took quite a bit of de-bugging as I had to figure out how to be able to add both text and the result (numberic) to the same line
 # Before I tried {}.format I was getting errors
 # Adding :.2f within curley braces rounds to two decimal places
