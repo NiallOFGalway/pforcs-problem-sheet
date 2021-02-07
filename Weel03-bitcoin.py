bdbcf308b68c8ff32b010eab19ffd2021898227f
NiallOFGalway <G00398819@gmit.ie>
2021-02-07 19:03:17 +0000

    Week 03 - Bitcoin

--- Weel03-bitcoin.py
+++ Weel03-bitcoin.py
@@ -6,7 +6,7 @@
 # This allows you to grab information from an external source
 # REFERENCE: https://realpython.com/python-json/cls
 # REFERENCE: https://www.w3schools.com/python/python_for_loops.asp
-# REFERENCE: https://github.com/hanumancode/Bitcoin-Price-Python
+# REFERENCE: https://github.com/hanumancode/Bitcoingit -Price-Python
 # REFERENCE: https://www.coindesk.com/coindesk-api
 
 import requests
@@ -20,3 +20,4 @@ returnedData = requests.get(url)
 print("Todays Bitcoin price in USD\t" + returnedData.json()['bpi']['USD']['rate'])
 print("Todays Bitcoin price in EUR\t" + returnedData.json()['bpi']['EUR']['rate'])
 print("Todays Bitcoin price in GBP\t" + returnedData.json()['bpi']['GBP']['rate'])
+# \t creates a tab which results in a tidier output
