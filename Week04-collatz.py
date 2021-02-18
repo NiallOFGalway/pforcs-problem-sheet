61eae29567f08575b3822661212b4913e8e26e00
NiallOFGalway <G00398819@gmit.ie>
2021-02-18 11:52:16 +0000

    Week 04 Collatz

--- Week04-collatz.py
+++ Week04-collatz.py
@@ -5,10 +5,15 @@
 
 # Reference: https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff
 # Reference: Andrew Beatty e-mail query
+# Reference: https://en.wikibooks.org/wiki/Python_Programming/Operators
+# Reference: https://stackoverflow.com/questions/20337489/python-how-to-keep-repeating-a-program-until-a-specific-input-is-obtained
+
+number = int(input("Enter a positive integer: "))
+# Requests the user to input a positive integer
 
 def thenumber(number):
     if number % 2 == 0:
-        (print(int(number / 2 )))
+        (print(number // 2 ))
 # If the number divided by 2 does not have a remainder, it is even.
 # Therefore return the even number divided by 2
     
@@ -16,15 +21,4 @@ def thenumber(number):
         return (print(int(number * 3 +1)))
 # Otherwise, return the number, multiply by 3 and add 1
 
-
-number = int(input("Enter a positive integer: "))
-# Requests the user to input a positive integer
-
 thenumber(number)
-
-
-
-
-
-
-
