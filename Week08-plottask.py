71185b32450feaca67989930cce169ea584992cf
NiallOFGalway <G00398819@gmit.ie>
2021-03-20 11:47:11 +0000

    Week08 - Plot using matplotlib

---
+++ Week08-plottask.py
@@ -0,0 +1,42 @@
+# Week08-plottask.py
+# Author: Niall O Flaherty
+# Write a program called plottask.py that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] on the one set of axes.
+# Reference: https://www.w3schools.com/python/matplotlib_pyplot.asp
+# Reference: https://realpython.com/python-matplotlib-guide
+# Reference: https://realpython.com/courses/python-histograms/
+# Reference: Andrew Beatty Week 08 Tutorial
+# Reference: https://matplotlib.org/2.0.2/users/pyplot_tutorial.html
+# Reference: https://stackoverflow.com/questions/16992038/inline-labels-in-matplotlib
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xpoints = np.array(range(0,4))
+# Creating the X-Axis 0,4
+
+fy = xpoints
+gy = xpoints * xpoints
+hy = xpoints * xpoints * xpoints
+# Creating the Y-Axis
+
+plt.plot(fy, linestyle='solid', linewidth='3', color='blue', label='fy')
+plt.plot(gy, linestyle='solid', linewidth='3', color='green', label='gy')
+plt.plot(hy, linestyle='solid', linewidth='3', color='red', label='hy')
+# Creates the f, g, y plot; the line-style; the line width; the colour; label 
+# Advised of extra marks for making the plot look nice, did not know how far to go with this!
+# plt.legend is required for the 'label' to show' (see below)
+
+plt.legend()
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+# Legend shows the names of the lines
+# plt.xlabel & plt.ylabel labels the X / Y Axis
+
+plt.show()
+# Outputs the plot on-screen
+
+'''
+plt.savefig('Week08-PlotImage.png')
+# The above code would save the output as a .png file
+'''
