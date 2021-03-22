d4294fef9108db03dc38380f3d10616fea7ee7d8
NiallOFGalway <G00398819@gmit.ie>
2021-03-22 13:38:56 +0000

    Week 09

---
+++ Week09.py
@@ -0,0 +1,14 @@
+# Week09.py
+# Author: Niall O Flaherty
+'''
+Find which sessionId downloaded the most data
+
+Read the access.log into a dataframe (see lab)
+Set the date time to be the index (you will need to manipulate this data, see lab)
+Use regular expressions to extract the session id from the URLs and store them in a different column (use the apply function, see lab)
+Use groupBy to get the sum of all the data downloaded by each sessionId.
+Create a plot of this (type of your choice)
+'''
+# Reference:
+# Reference:
+# Reference:
