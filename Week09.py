35bde8c4b2d3d4d9787f33e61229a3097a7bc447
NiallOFGalway <G00398819@gmit.ie>
2021-04-08 14:16:53 +0100

    Week 09

--- Week09.py
+++ Week09.py
@@ -9,7 +9,7 @@ Use regular expressions to extract the session id from the URLs and store them i
 Use groupBy to get the sum of all the data downloaded by each sessionId.
 Create a plot of this (type of your choice)
 '''
-# Reference:
+# Reference: Andrew Beatty Week09 Lab
 # Reference:
 # Reference:
 
@@ -20,3 +20,18 @@ import matplotlib.pyplot as plot
 # File to read data from
 filename = "Week09-access.log"
 
+# Add column names (Credit: Andrew Beatty Week09 Lab)
+colNames = ('ip',
+            'dash',
+            'user',
+            'time',
+            'request',
+            'status code',
+            'size of response',
+            'referer',
+            'user agent',
+            'dunno'
+            )
+
+# Reads the file into a dataframe (Credit: Andrew Beatty Week09 Lab)
+df = pd.read_csv(filename, sep=' ', header= None, names=colNames)
